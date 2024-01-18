cbbd87c358e644b0a7336bb61fe1eeceabcbb08d
Betül Sarı <52050427+Bet06sari@users.noreply.github.com>
2024-01-18 19:53:35 +0300

    file system attempt

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res){
+    if (req.url == "/"){
+        fs.readFile("index.html", function(err,html){
+            res.write(html);
+            res.end();
+        });
+    }else if (req.url== "/product"){
+        fs.readFile("product.html", function(err,html){
+            res.write(html);
+            res.end();
+        });
+    }else {
+        fs.readFile("404.html", function(err,html){
+            res.write(html);
+            res.end();
+        });
+    }
+});
+
+server.listen(3000, ()=>{
+    console.log("Server is running on port 3000\n");
+});
